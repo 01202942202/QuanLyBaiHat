1329dfdf16a629431cfc0d81f46e6720fc8a9c0d
pnxphuc <1451010135phuc@ou.edu.vn>
2017-03-27 11:28:34 +0700

    FormMain

---
+++ QuanLyBaiHat/QuanLyBaiHat/frmMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace QuanLyBaiHat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void thôngTinSinhViênThựcHiệnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Các sinh viên thực hiện:\nPhạm Nguyễn Xuân Phúc - 1451010135\nTrần Dũng Danh - 1451010025\nLê Thanh Phước - 1451010143\nTrương Quang Tân - 1451010172");
+        }
+    }
+}
